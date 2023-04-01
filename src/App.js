c2a6b7c6bf16fa9fd46cbe6b945659dfe76b64b0
avikodre03 <akodre111@gmail.com>
2023-04-01 20:58:25 +0530

    movie_app done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+import { createContext } from "react";
+
+
+import Header from "./Components/Header/Header";
+import MovieList from "./Components/movieList/MovieList";
+import MovieDetails from "./Pages/movieDetails/MovieDetails";
+
+import { getMoviesLIst } from "./Functions/getMoviesList";
+
+import Home from "./Pages/home/Home";
+
+export const globalData = createContext();
 
 function App() {
+  const [popularMoviesFromApi, setPopularMoviesFromApi] = useState([]);
+  const [upcomingMoviesFromApi, setUpcomingMoviesFromApi] = useState([]);
+  const [topRatedMoviesFromApi, setTopRatedMoviesFromApi] = useState([]);
+  const [state, setstate] = useState([]);
+
+  useEffect(() => {
+    getMoviesLIst(setPopularMoviesFromApi, "popular");
+    getMoviesLIst(setTopRatedMoviesFromApi, "top_rated");
+    getMoviesLIst(setUpcomingMoviesFromApi, "upcoming");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <globalData.Provider
+      value={{
+        popularMoviesFromApi: popularMoviesFromApi,
+        topRatedMoviesFromApi: topRatedMoviesFromApi,
+        upcomingMoviesFromApi: upcomingMoviesFromApi,
+        state, setstate
+      }}
+    >
+      <div className="App">
+        <Router>
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="movie/:id" element={<MovieDetails/>}></Route>
+            <Route path="movies/:category" element={<MovieList/>}></Route>
+            <Route path="/*" element={<h1>Page Not Found</h1>}></Route>
+          </Routes>
+        </Router>
+      </div>
+    </globalData.Provider>
   );
 }
 
